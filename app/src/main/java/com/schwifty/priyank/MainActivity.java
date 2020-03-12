25f98e9e591cac760aa0e134da03e396eff6c04b
shubham <coldboy1998@gmail.com>
2020-03-13 00:24:43 +0530

    plus_search_final.xml completed

--- app/src/main/java/com/schwifty/priyank/MainActivity.java
+++ app/src/main/java/com/schwifty/priyank/MainActivity.java
@@ -12,21 +12,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.suggestedstudents_final);
+        setContentView(R.layout.plus_search_final);
 
         LayoutInflater inflater = LayoutInflater.from(this);
-        LinearLayout ParentElementGroups = findViewById(R.id.suggestedgroups_final_suggested_students_container);
-//        LinearLayout ParentElementStudents = findViewById(R.id.plus_final_suggested_students_container);
-//
-//
+        LinearLayout ParentElementGroups = findViewById(R.id.UniversityContainer);
+
         for(int i = 0; i< 14; i++) {
-            View mView = inflater.inflate(R.layout.inflate_suggestedstds, null, false);
+            View mView = inflater.inflate(R.layout.inflate_suggestedgps, null, false);
             ParentElementGroups.addView(mView);
         }
-//
-//        for(int i = 0; i< 4; i++) {
-//            View mView = inflater.inflate(R.layout.inflate_suggestedstds, null, false);
-//            ParentElementStudents.addView(mView);
-//        }
+
     }
 }
