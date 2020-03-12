f37cdc685a1534ff572474c29e4edcd28addc497
shubham <coldboy1998@gmail.com>
2020-03-13 01:22:44 +0530

    group_chat_final.xml completed

--- app/src/main/java/com/schwifty/priyank/MainActivity.java
+++ app/src/main/java/com/schwifty/priyank/MainActivity.java
@@ -6,20 +6,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plus_search_final);
+        setContentView(R.layout.group_chat_final);
 
         LayoutInflater inflater = LayoutInflater.from(this);
-        LinearLayout ParentElementGroups = findViewById(R.id.UniversityContainer);
+        LinearLayout ParentElementGroups = findViewById(R.id.chat_container);
 
-        for(int i = 0; i< 14; i++) {
-            View mView = inflater.inflate(R.layout.inflate_suggestedgps, null, false);
-            ParentElementGroups.addView(mView);
+        String[] names = new String [] {"Priyank", "Ranjit", "Soumya"};
+
+        String[] arr = new String [] {"Hello", "Hi", "Have you guys booked your visa appointment already",
+        "Yes I did","Yeah me too!", "yes","When is your appointment and in which city?",
+        "2nd April, Canadian Embassy New Delhi", "Mine is on 5th April in Mumbai","Yeah, Sure" };
+
+        for(int i = 0; i< arr.length; i++) {
+
+            if(i%2 == 0) {
+                View mView = inflater.inflate(R.layout.inflate_chat_me, null, false);
+                ((TextView)mView.findViewById(R.id.mine_message)).setText(arr[i]);
+                ParentElementGroups.addView(mView);
+            }
+            else
+            {
+                View mView = inflater.inflate(R.layout.inflate_chat_others, null, false);
+                ((TextView)mView.findViewById(R.id.others_message)).setText(arr[i]);
+                ((TextView)mView.findViewById(R.id.others_name)).setText(names[i%3]);
+
+                ParentElementGroups.addView(mView);
+            }
         }
 
     }
