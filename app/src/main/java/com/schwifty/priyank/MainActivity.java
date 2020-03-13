aaee7fad70bf156d2bda4836af8f1797ba1fe814
shubham <coldboy1998@gmail.com>
2020-03-13 19:07:08 +0530

    onboarding - instead of inflating a template, replaced it with layout files

--- app/src/main/java/com/schwifty/priyank/MainActivity.java
+++ app/src/main/java/com/schwifty/priyank/MainActivity.java
@@ -3,28 +3,18 @@ package com.schwifty.priyank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageListener;
 import com.synnapps.carouselview.ViewListener;
 
 public class MainActivity extends AppCompatActivity {
 
     CarouselView customCarouselView;
-    int NUMBER_OF_PAGES = 3;
+    int [] onboardScreens = {R.layout.inflate_onboard1,R.layout.inflate_onboard2,R.layout.inflate_onboard3};
 
-    int[] onboardingImg = {R.drawable.onboarding1,R.drawable.onboarding2,R.drawable.onboarding1};
-
-    String[] onboardingTxt1= {"Exclusive Community","Connect with seniors & aspirants","Everything you need"};
-
-    String[] onboardingTxt2 = {"Community of study abroad aspirants, admits and seniors. No Spammers!",
-           "Complete your profile and connect with similar student profiles",
-            "Everything you need for your abroad education all in one place"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.onboard);
 
         customCarouselView = (CarouselView) findViewById(R.id.carouselView);
-        customCarouselView.setPageCount(NUMBER_OF_PAGES);
+        customCarouselView.setPageCount(onboardScreens.length);
         // set ViewListener for custom view
         customCarouselView.setViewListener(viewListener);
     }
@@ -41,16 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View setViewForPosition(int position) {
-            View customView = getLayoutInflater().inflate(R.layout.inflate_onboard, null);
-
-            TextView labelTextView1 = (TextView) customView.findViewById(R.id.onboarding_text1);
-            TextView labelTextView2 = (TextView) customView.findViewById(R.id.onboarding_text2);
-            ImageView imageView = (ImageView) customView.findViewById(R.id.onboarding_image);
-
-            imageView.setImageResource(onboardingImg[position]);
-            labelTextView1.setText(onboardingTxt1[position]);
-            labelTextView2.setText(onboardingTxt2[position]);
-
+            View customView = getLayoutInflater().inflate(onboardScreens[position], null);
             return customView;
         }
     };
